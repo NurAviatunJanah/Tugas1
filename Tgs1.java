d5c16821183ca12cbd14705424e049f1d8340c64
NurAviatunJanah <48704040+NurAviatunJanah@users.noreply.github.com>
2019-04-20 11:37:08 +0700

    Create Tgs1.java

---
+++ Tgs1.java
@@ -0,0 +1,8 @@
+public class  Cavia {
+    public static void main(String[] args){
+        System.out.println("Nama    : Nur Aviatun Janah");
+        System.out.println("NIM     : 17090091");
+        System.out.println("Kelas   : 4C");
+    }
+
+}
